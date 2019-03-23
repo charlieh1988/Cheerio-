7fae1065292f6d4a402e705f3eea190f707ca8f4
Charlie Hilton <charllie.hiltons@gmail.com>
2019-03-23 10:23:12 -0500

    revamp

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 // Exporting an object containing all models
 
 module.exports = {
-    Articles: require("./Articles.js"),
-    Notes: require("./Comments.js")
-  };
-  
+  Articles: require("./Articles.js"),
+  Notes: require("./Comments.js")
+};
